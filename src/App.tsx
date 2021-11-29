a2d5888779bb2a88fda5b1bb2a45ff5f98836cdb
Manu MA <manu.mtza@gmail.com>
2021-11-29 20:21:04 +0100

    ✨

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { BuilderComponent } from '@builder.io/react';
 function App() {
   return (
     <div className="App">
-      <BuilderComponent model="page" />
+      <BuilderComponent model="Template" entry="cb228978a2d54111a0c4ebc62ea20c4c" />
     </div>
   );
 }
